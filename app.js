bb4cf399f1805f35cfd978a37b1833137e245dc0
BentleyDombrow <bentley.dombrow@gmail.com>
2023-01-25 17:19:36 -0500

    App rendering and able to display some basic versions of the game

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express();
+
+const horses = ['geddy', 'neil', 'alex', 'stang', 'pierce'];
+
+app.set('view engine', 'ejs');
+
+app.use("/public", express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('index', {horseNames: horses});
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,3 @@
+let parentFile = $(".Parent");
+
+console.log(parentFile);
